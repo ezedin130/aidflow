4b4bd625a0cbe1271002f8d460365edfba76e545
Ezedi <ezedinawol130@gmail.com>
2025-11-10 20:17:35 +0300

    user class that is used as entity model

---
+++ src/main/java/com/example/aidflow/model/User.java
@@ -0,0 +1,36 @@
+package com.example.aidflow.model;
+
+import com.example.aidflow.constant.RoleType;
+import com.example.aidflow.constant.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
