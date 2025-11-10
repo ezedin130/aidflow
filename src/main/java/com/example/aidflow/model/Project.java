aa321fcc1ee01c490c424de2182a17a0ecca0159
Ezedi <ezedinawol130@gmail.com>
2025-11-10 20:17:54 +0300

    project class that is used as entity model

---
+++ src/main/java/com/example/aidflow/model/Project.java
@@ -0,0 +1,45 @@
+package com.example.aidflow.model;
+
+import com.example.aidflow.constant.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private double targetAmount;
+    @Column(nullable = false)
+    private double collectedAmount;
+    @Column(nullable = false)
+    private String location;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProjectStatus status;
+    @Column(nullable = false)
+    private LocalDate startDate;
+    @Column(nullable = false)
+    private LocalDate endDate;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ngo_id")
+    private Ngo ngo;
+}
