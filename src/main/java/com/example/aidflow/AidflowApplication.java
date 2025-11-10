00c351a2ad515c17e5be59a034e03374580745a6
Ezedi <ezedinawol130@gmail.com>
2025-11-10 16:44:06 +0300

    new initial commit

---
+++ src/main/java/com/example/aidflow/AidflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aidflow/AidflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
