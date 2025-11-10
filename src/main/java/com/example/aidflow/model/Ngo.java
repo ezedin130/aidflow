30d1de09962fd1bcea8f3dc88fbb93f80870cd88
Ezedi <ezedinawol130@gmail.com>
2025-11-10 20:17:41 +0300

    ngo class that is used as entity model

---
+++ src/main/java/com/example/aidflow/model/Ngo.java
@@ -0,0 +1,39 @@
+package com.example.aidflow.model;
+
+import com.example.aidflow.constant.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ngo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String mission;
+    //logo field
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String phone;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
