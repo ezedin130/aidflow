743db2c11e2fa3e5851ba73ac9e567db6690f875
Ezedi <ezedinawol130@gmail.com>
2025-11-10 20:37:35 +0300

    dtos(data transfer objects) for ngo

---
+++ src/main/java/com/example/aidflow/dto/ngoDto/NgoInDto.java
@@ -0,0 +1,13 @@
+package com.example.aidflow.dto.ngoDto;
+
+import lombok.Data;
+
+@Data
+public class NgoInDto {
+    private String name;
+    private String mission;
+    private String address;
+    private String phone;
+    private String status;
+    private Long user;
+}
---
+++ src/main/java/com/example/aidflow/dto/ngoDto/NgoOutDto.java
@@ -0,0 +1,17 @@
+package com.example.aidflow.dto.ngoDto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class NgoOutDto {
+    private Long id;
+    private String name;
+    private String mission;
+    private String address;
+    private String phone;
+    private String status;
+    private LocalDateTime createdAt;
+    private Long user;
+}
