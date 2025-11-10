0b256abee639473b4a86f8ed68a065d921375d66
Ezedi <ezedinawol130@gmail.com>
2025-11-10 20:23:46 +0300

    user Distribution interface class used as repository for the Resource user model

---
+++ src/main/java/com/example/aidflow/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.aidflow.repository;
+
+import com.example.aidflow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
