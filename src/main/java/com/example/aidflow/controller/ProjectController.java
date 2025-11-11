f685d96da86c47852d6f55aa40b0ceca82d98abe
Ezedi <ezedinawol130@gmail.com>
2025-11-12 00:43:26 +0300

    new endpoints to update the project details

--- src/main/java/com/example/aidflow/controller/ProjectController.java
+++ src/main/java/com/example/aidflow/controller/ProjectController.java
@@ -1,9 +1,11 @@
 package com.example.aidflow.controller;
 
+import com.example.aidflow.constant.ProjectStatus;
 import com.example.aidflow.dto.projectDto.ProjectInDto;
 import com.example.aidflow.dto.projectDto.ProjectOutDto;
 import com.example.aidflow.mapper.ProjectMapper;
 import com.example.aidflow.model.Project;
+import com.example.aidflow.repository.ProjectRepository;
 import com.example.aidflow.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -24,11 +27,32 @@ public class ProjectController {
     private final ProjectService service;
     @Autowired
     private final ProjectMapper mapper;
+    @Autowired
+    private final ProjectRepository projectRepo;
 
     @PostMapping("/create-project")
     public ResponseEntity<ProjectOutDto> createProject(@RequestBody ProjectInDto dto){
         ProjectOutDto result = service.createProject(dto);
         return ResponseEntity.ok(result);
+    }
+    @PutMapping("/{id}/activate")
+    //@PreAuthorize(("hasRole('ADMIN')"))
+    public ResponseEntity<ProjectOutDto> activateProject(@PathVariable Long id){
+        Project project = projectRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Project not found"));
+
+        project.setStatus(ProjectStatus.ACTIVE);
+        project.setStartDate(LocalDate.now());
+
+        Project updated = projectRepo.save(project);
+        ProjectOutDto result = mapper.toDto(updated);
+        return ResponseEntity.ok(result);
+    }
+    @PutMapping("/{id}/complete")
+    //@PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ProjectOutDto> completeProject(@PathVariable Long id) {
+        ProjectOutDto updatedProject = service.completeProject(id);
+        return ResponseEntity.ok(updatedProject);
     }
     @GetMapping()
     public ResponseEntity<List<ProjectOutDto>> findAllProject(){
