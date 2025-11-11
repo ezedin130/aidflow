8a26513854568491f05e78f1c2148a9ef4ade143
Ezedi <ezedinawol130@gmail.com>
2025-11-12 00:36:55 +0300

    enum used for as type of resource unit type

---
+++ src/main/java/com/example/aidflow/constant/ResourceUnitType.java
@@ -0,0 +1,7 @@
+package com.example.aidflow.constant;
+
+public enum ResourceUnitType {
+    MONEY,          // $ or other currency
+    COUNTABLE_GOOD, // Items you can count (blankets, tents, bottles)
+    WEIGHTED_GOOD   // Items measured in kg/liters/etc. (rice, oil, water)
+}
