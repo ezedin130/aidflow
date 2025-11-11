08d7ad9e444af1e1dd73d9c216dc176dde85cab6
Ezedi <ezedinawol130@gmail.com>
2025-11-11 16:16:21 +0300

    security configuration implemented

---
+++ src/main/java/com/example/aidflow/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.aidflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(
+                        AbstractHttpConfigurer :: disable
+                )
+                .authorizeHttpRequests(request -> request
+                        //.requestMatchers()
+                        //.permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
