c49a58cc1745fecd2b07233d7655b8d95c77bee0
Ezedi <ezedinawol130@gmail.com>
2025-11-11 13:49:35 +0300

    the generation type changed from auto to identity

--- src/main/java/com/example/aidflow/model/Ngo.java
+++ src/main/java/com/example/aidflow/model/Ngo.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Ngo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
--- src/main/java/com/example/aidflow/model/ResourceDistribution.java
+++ src/main/java/com/example/aidflow/model/ResourceDistribution.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ResourceDistribution {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String resourceName;
