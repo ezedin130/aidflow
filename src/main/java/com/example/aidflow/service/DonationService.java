efce7f787bcc79b20bda77b32a125c37ec947aac
Ezedi <ezedinawol130@gmail.com>
2025-11-12 00:39:16 +0300

    checks if the user that donates has donor role and also updates the project status based on its requirements

--- src/main/java/com/example/aidflow/service/DonationService.java
+++ src/main/java/com/example/aidflow/service/DonationService.java
@@ -1,5 +1,7 @@
 package com.example.aidflow.service;
 
+import com.example.aidflow.constant.ProjectStatus;
+import com.example.aidflow.constant.RoleType;
 import com.example.aidflow.dto.donationDto.DonationInDto;
 import com.example.aidflow.dto.donationDto.DonationOutDto;
 import com.example.aidflow.mapper.DonationMapper;
@@ -12,7 +14,9 @@ import com.example.aidflow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,14 +31,24 @@ public class DonationService {
     private final ProjectRepository  projectRepo;
     @Autowired
     private final UserRepository userRepo;
-
+    @Transactional
     public DonationOutDto createDonation(DonationInDto dto){
         Project project = projectRepo.findById(dto.getProject())
                 .orElseThrow(()-> new RuntimeException("Project not found"));
         User user = userRepo.findById(dto.getUser())
                 .orElseThrow(()-> new RuntimeException("User not found"));
+        if (user.getRole() != RoleType.DONOR) {
+            throw new IllegalStateException("User must have the DONOR role to create an Donation profile");
+        }
         Donation donation = mapper.toEntity(dto,user,project);
         Donation savedDonation = donationRepo.save(donation);
+        double newCollectedAmount = project.getCollectedAmount() + donation.getAmount();
+        project.setCollectedAmount(newCollectedAmount);
+
+        if (project.getStatus() == ProjectStatus.PENDING && newCollectedAmount >= project.getTargetAmount()) {
+            project.setStatus(ProjectStatus.ACTIVE);
+            project.setStartDate(LocalDate.now());
+        }
         return mapper.toDto(savedDonation);
     }
     public List<DonationOutDto> getAllDonations(){
